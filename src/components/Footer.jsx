722e37bd7ea8dd910746231b97fab706382cc300
atheo810 <atheos810@gmail.com>
2025-02-28 12:13:32 +0800

    feat: components Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h3>Happy Travel</h3>
+        <h3>and Tours.</h3>
+        <p>
+          Nikmati liburan nyaman bersama kami, segera pesan tiketmu sekarang
+          juga.
+        </p>
+      </div>
+
+      <div>
+        <h3>Kontak kami</h3>
+        <p>Telp: 08123456789</p>
+        <p>WA: 08123456789</p>
+        <p>Email: travel@gmail.com</p>
+      </div>
+
+      <div>
+        <h3>Ikuti kami</h3>
+        {SOCIALS.map(({ label, href }) => (
+          <p key={label}>
+            {label}: <a href={href}>@{label.toLowerCase()}</a>
+          </p>
+        ))}
+      </div>
+    </footer>
+  );
+}
